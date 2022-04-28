88f12eeca20d960113562667c8c40e0d51874a5d
User <User@DEIV>
2022-04-27 22:40:04 -0500

    Agregar archivos de proyecto.

---
+++ CabezasS2/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CabezasS2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnLogin_Clicked(object sender, EventArgs e)
+        {
+            String Usuario = txtUsuario.Text;
+            String Clave = txtClave.Text;
+            if ((Usuario == "estudiante2021") && (Clave == "uisrael2021"))
+            {
+                await Navigation.PushAsync(new TareaS2(Usuario));
+                            }
+            else
+            {
+                await DisplayAlert("Mensaje de alerta", "Datos Incorrectos", "Cerrar");
+                
+            }
+
+        }
+    }
+}
---
+++ CabezasS2/TareaS2.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CabezasS2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TareaS2 : ContentPage
+    {
+        public TareaS2(String Usario)
+        {
+            InitializeComponent();
+            lblUsuConectado.Text = Usario;
+
+        }
+
+        private void btnCalcular_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                double parcial1 = Convert.ToDouble(txtParcial1.Text);
+                double parcial2 = Convert.ToDouble(txtParcial2.Text);
+                double examenP1 = Convert.ToDouble(txtExamenP1.Text);
+                double examenP2 = Convert.ToDouble(txtExamenP2.Text);
+
+                double promedioP1 = (parcial1 * 0.3) + (examenP1 * 0.2);
+                
+                double promedioP2 = (parcial2 * 0.3) + (examenP2 * 0.2);
+
+                txtTotal1.Text = promedioP1.ToString();
+                
+                txtTotal2.Text = promedioP2.ToString();
+
+                double promediofinal = promedioP1 + promedioP2;
+
+                txtPFinal.Text = promediofinal.ToString();
+
+                if (Convert.ToDouble(txtPFinal.Text) >= 7)
+                {
+                    DisplayAlert("Mensaje de alerta", "Aprobado", "Cerrar");
+                }
+                else
+                {
+                    if (Convert.ToDouble(txtPFinal.Text) >= 5 && Convert.ToDouble(txtPFinal.Text) <= 6.9)
+                    {
+                        DisplayAlert("Mensaje de alerta", "Complementario", "Cerrar");
+                    }
+
+                    DisplayAlert("Mensaje de alerta", "Reprobado", "Cerrar");
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                DisplayAlert("ERROR", ex.Message, "Cerrar");
+
+            }
+        }
+    }
+}
